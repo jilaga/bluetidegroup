03f5a35319a2fa1c32a6362526749ab18593be83
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-09-10 21:05:15 +0100

    style: innovations font-size

--- app/components/landing/Innovation.tsx
+++ app/components/landing/Innovation.tsx
@@ -14,28 +14,28 @@ export default function Innovation() {
   return (
     <div ref={containerRef} className="grid w-full h-[300vh] ">
       <div className="sticky top-0 flex items-center h-screen overflow-hidden">
-        <motion.div className="flex w-full" style={{ x }}>
+        <motion.div className="flex w-full font-clash" style={{ x }}>
           <div className="p-4 grid min-h-80 place-content-center text-center font-medium w-full shrink-0">
-            <p className="text-[#FFB98A] text-xl">
+            <p className="text-[#FFB98A] text-xl md:text-2xl font-normal">
               To our clients and partners
             </p>
-            <p className="text-[#FF6700] text-2xl">
+            <p className="text-[#FF6700] text-2xl md:text-4xl font-semibold">
               we offer COMPETENCE & INTEGRITY
             </p>
           </div>
           <div className="p-4 grid min-h-80 place-content-center text-center font-medium w-full shrink-0">
-            <p className="text-[#FFB98A] text-xl">
+            <p className="text-[#FFB98A] text-xl md:text-2xl font-normal">
               To our clients and partners
             </p>
-            <p className="text-[#FF6700] text-2xl">
+            <p className="text-[#FF6700] text-2xl md:text-4xl font-semibold">
               we offer COMPETENCE & INTEGRITY
             </p>
           </div>
           <div className="p-4 grid min-h-80 place-content-center text-center font-medium w-full shrink-0">
-            <p className="text-[#FFB98A] text-xl">
+            <p className="text-[#FFB98A] text-xl md:text-2xl font-normal">
               To our clients and partners
             </p>
-            <p className="text-[#FF6700] text-2xl">
+            <p className="text-[#FF6700] text-2xl md:text-4xl font-semibold">
               we offer COMPETENCE & INTEGRITY
             </p>
           </div>
