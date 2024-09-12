4c16b87b15fcf890f465f2f23555b1cae66c270a
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-09-12 17:07:14 +0100

    fix: service cards

--- app/components/landing/Services.tsx
+++ app/components/landing/Services.tsx
@@ -13,14 +13,19 @@ const ServiceCard: React.FC<{
 }> = ({ title, description, imageSrc, delay }) => {
   return (
     <ResponsiveScrollSwipe delay={delay / 1000}>
-      <motion.div
-        className=" w-full rounded-2xl h-[20rem] md:h-[650px] md:w-[600px] flex flex-col items-end justify-end p-4 md:pt-[2.5rem] sm:pb-[3rem] sm:pl-[3rem] sm:pr-[2.5rem] 
-          box-border gap-[0.5rem] bg-cover object-fit bg-[top] relative overflow-hidden
-          transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl "
-        style={{ backgroundImage: `url(${imageSrc})` }}
-      >
+      <motion.div className="rounded-2xl  flex flex-col items-end justify-end p-4 md:pt-[2.5rem] sm:pb-[3rem] sm:pl-[3rem] sm:pr-[2.5rem] box-border gap-[0.5rem] bg-cover object-fit bg-[top] relative isolate overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl aspect-[600/650] group cursor-pointer">
+        <div className="absolute inset-0">
+          <Image
+            src={imageSrc}
+            className="w-full h-full object-cover"
+            alt="background"
+            width={600}
+            height={650}
+          />
+          <div className="absolute inset-0 bg-gradient-to-b from-black to-[#011123] opacity-60" />
+        </div>
         <div className="relative flex justify-end z-10">
-          <div className="relative cursor-pointer p-2 rounded-full z-10 bg-[#FF6700]">
+          <div className="relative cursor-pointer p-2 rounded-full z-10 border-white border group-hover:bg-[#FF6700] group-hover:border-none animate-[200ms_linear_background-color]">
             <HiArrowUpRight className="size-6 sm:size-8 overflow-hidden shrink-0 fill-white" />
           </div>
         </div>
@@ -39,26 +44,19 @@ const ServiceCard: React.FC<{
 
 export default function OurServicesPage() {
   const containerRef = useRef<HTMLDivElement>(null);
-  const { scrollYProgress } = useScroll({
-    target: containerRef,
-    offset: ['end end', 'start start'],
-  });
-  const size = useTransform(() => `${scrollYProgress.get() * 100}%`);
+  // const { scrollYProgress } = useScroll({
+  //   target: containerRef,
+  //   offset: ['end end', 'start start'],
+  // });
+  // const size = useTransform(() => `${scrollYProgress.get() * 100}%`);
 
   return (
     <div
       ref={containerRef}
-      className="relative flex top-4 flex-col justify-start items-center text-left text-[2.25rem] m-4 md:m-20 gap-4 md:gap-10 "
+      className="relative w-full flex top-4 flex-col justify-start items-center text-left text-[2.25rem] m-4 md:m-20 gap-4 md:gap-10"
     >
       <div className="sticky top-0 w-full h-[100vh] min-h-screen">
-        <motion.div
-          style={{
-            width: size,
-            height: size,
-            opacity: size,
-          }}
-          className="absolute inset-[50%_50%_auto_auto] translate-x-1/2 -translate-y-1/2"
-        >
+        <motion.div className="absolute inset-0">
           <Image
             src="/service/bg.png"
             width={1440}
@@ -68,11 +66,11 @@ export default function OurServicesPage() {
           />
         </motion.div>
       </div>
-      <div className="w-full relative flex flex-col md:flex-row gap-[2rem] md:gap-[5rem] p-4  ">
+      <div className="w-full relative flex flex-col md:flex-row gap-[2rem] md:gap-[5rem] p-4 lg:px-[80px]">
         <div className="w-full relative flex flex-col gap-[2rem] md:gap-[5rem]">
           <ResponsiveScrollSwipe>
             <div
-              className="self-stretch h-[20rem] md:h-[650px] min-w-[18rem] md:w-[600px] rounded-2xl flex flex-col items-start justify-start p-5 box-border gap-4 md:gap-6 bg-cover relative
+              className="self-stretch aspect-[600/650] rounded-2xl flex flex-col items-start justify-start p-5 box-border gap-4 md:gap-6 bg-cover relative
              bg-[#FF8533] hover:bg-orange-500 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl"
             >
               <p className="text-[2rem] md:text-[2.5rem] md:mb-4 self-stretch relative leading-[130%] font-semibold md:font-normal text-foundation-rust-accent-rust-accent-50 font-clash">
@@ -98,7 +96,6 @@ export default function OurServicesPage() {
             imageSrc="/service/serv1.png"
             delay={0}
           />
-
           <ServiceCard
             title="Electrical Installation"
             description="Expert electrical installation services for marine vessels"
