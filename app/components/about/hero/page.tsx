4f9c9052a80fa3a1c0786fd28295e3ab56119b14
Avioflagos <120939905+AviOfLagos@users.noreply.github.com>
2024-09-13 10:15:40 +0100

    About Us: fixed Hero
    Removed h-screen, adjusted responsiveness

--- app/components/about/hero/page.tsx
+++ app/components/about/hero/page.tsx
@@ -3,24 +3,26 @@ import React from 'react';
 
 export default function HeroSec() {
   return (
-    <section className="w-full h-screen flex justify-center items-center">
+    <section className="w-full flex justify-center items-center">
       <ScrollFade
         threshold={0.1}
         duration={0.2}
         delay={0.2}
-        className="w-full relative flex sm:flex-row flex-col items-start justify-center pt-[16rem] px-[1.5rem] sm:px-[5rem] pb-[3rem] sm:pb-[5rem] box-border gap-[2.5rem] sm:gap-[7.75rem] text-left  text-foundation-grey-grey-100 font-clash "
+        className="w-full relative flex sm:flex-row flex-col items-start justify-center 
+          pt-[20rem] px-[1.5rem] md:px-[2.5rem] lg:px-[5rem] pb-[2.5rem]
+          gap-[2.5rem] lg:gap-[7.5rem] text-left text-foundation-grey-grey-100 font-clash"
       >
-        <div className="w-full sm:w-[14rem] relative tracking-[0.03em] leading-[140%] uppercase font-medium ">
-          <p className="mt-5 sm:text-[1.25rem] text-[0.875rem] text-nowrap">
+        <div className="w-auto relative tracking-[0.03em] leading-[140%] uppercase font-medium ">
+          <p className="mt-5 md:text-[1.25rem] text-[0.875rem] text-nowrap font-medium font-title-1-semibold">
             Subsea Equipment Rental
           </p>
         </div>
-        <div className="w-full  flex flex-col items-start justify-start gap-[2.5rem] text-[2rem] sm:text-[3.5rem] text-foundation-grey-grey-400 font-satoshi">
-          <b className="self-stretch relative leading-[140%] font-semibold font-clash whitespace-pre-wrap">
+        <ScrollFade className="w-full  flex flex-col items-start justify-start gap-[2.5rem] text-foundation-grey-grey-400 font-satoshi">
+          <b className="relative text-[2.25rem] lg:text-[4.5rem] leading-[130%] lg:leading-[140%] font-semibold font-clash whitespace-pre-wrap">
             We have successfully and skillfully managed all operations and
             services in line with the highest industry standards.
           </b>
-        </div>
+        </ScrollFade>
       </ScrollFade>
     </section>
   );
