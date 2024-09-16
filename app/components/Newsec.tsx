e7a41b798ee1115cd818d6de5f7f443e3c1750e9
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-09-16 14:10:40 +0100

    chore: removed comments

--- app/components/Newsec.tsx
+++ app/components/Newsec.tsx
@@ -3,10 +3,7 @@
 import Image from 'next/image';
 import React from 'react';
 import { motion } from 'framer-motion';
-// import { Button } from 'antd';
 import { HiOutlineArrowDownCircle } from 'react-icons/hi2';
-// import HeroImg from './../../public/hero.png';
-// import ScrollFade from '@/utils/SlideFade.jsx';
 
 export default function Newsec() {
   return (
@@ -52,55 +49,4 @@ export default function Newsec() {
       </div>
     </section>
   );
-}
-
-{
-  /* <section className="w-full grid grid-cols-[1fr] grid-rows-[1fr] relative h-[100dvh] px-[1rem] md:px-[3rem] sm:px-[5rem] pb-[20dvh] sm:pb-[5rem]">
-      <div className="absolute inset-[0_0_0_0] -z-[1] bg-[#0050AA]">
-        <Image
-          src={HeroImg}
-          alt="Hero background"
-          width={1280}
-          height={960}
-          object-fit="cover"
-          className="h-full w-full object-cover z-1 opacity-[63%]"
-        />
-      </div>
-      <div className="max-w-[1100px] mt-auto row-start-1 col-start-1">
-        <ScrollFade
-          threshold={0.2}
-          duration={0.7}
-          delay={0.1}
-          className="flex flex-col w-full h-full justify-end gap-10 "
-        >
-          <h1 className="flex items-end w-full max-w-[344px] sm:max-w-[855px] text-[2.5rem] sm:text-[4.5rem] leading-[140%] font-semibold font-clash text-foundation-rust-accent-rust-accent-50">
-            Expertise You Can Trust, Excellence You Can See
-          </h1>
-          <div className="w-full flex justify-start sm:justify-end items-center pb-[40px] ">
-            <div className="flex flex-col justify-center items-start sm:items-start w-[24rem] md:w-[400px] gap-4 ">
-              <p className="w-[270px] pl-1 text-[1rem] leading-normal tracking-wide text-foundation-rust-accent-rust-accent-50 text-left font-semibold">
-                Delivering superior solutions for
-                <span className="text-[#ec9762]">
-                  {' '}
-                  Underwater Hull Cleaning, Diving
-                </span>{' '}
-                and
-                <span className="text-[#ec9762]"> ROV Services.</span>
-              </p>
-              <button className="gap-2 hidden sm:flex flex-row items-center justify-center box-border text-left text-[1rem] text-linen ">
-                Explore more
-                <HiOutlineArrowDownCircle className="w-6 h-6" />
-              </button>
-              <Button
-                href="#"
-                size="large"
-                className="w-full h-[4.5rem] rounded-[99px] sm:hidden bg-foundation-rust-accent-rust-accent-500 text-[20px] text-linen"
-              >
-                Let’s talk
-              </Button>
-            </div>
-          </div>
-        </ScrollFade>
-      </div>
-    </section> */
 }
