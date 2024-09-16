dbb2c121451fe1257b4c48a202eff4291a7ad81a
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-09-16 18:55:48 +0100

    feat: our services navigation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,16 +1,43 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect, useMemo, useRef, MouseEventHandler } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { motion } from 'framer-motion';
+import { motion, animate, motionValue } from 'framer-motion';
 import { FiMenu, FiX } from 'react-icons/fi';
 import MobileNav from './MobileNav';
 import { twMerge } from 'tailwind-merge';
+import { HiMiniArrowUpRight } from 'react-icons/hi2';
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [areServicesOpen, setAreServicesOpen] = useState(false);
+
+  const serviceMenuRef = useRef<HTMLDivElement>(null);
+
+  const opacity = useMemo(() => motionValue(0), []);
+  const handleOuterClick: (this: Document, ev: MouseEvent) => any = function (
+    e
+  ) {
+    if (!serviceMenuRef.current!.contains(e.target as Node)) {
+      setAreServicesOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleOuterClick, true);
+  }, []);
+  useEffect(() => {
+    if (areServicesOpen) {
+      animate(opacity, 1, { duration: 0.3 });
+      console.log('to 1');
+      return;
+    }
+    console.log('to 0');
+    animate(opacity, 0, { duration: 0.3 });
+  }, [areServicesOpen]);
+
   const pathname = usePathname();
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
@@ -19,6 +46,17 @@ export default function Navbar() {
     <>
       <MobileNav />
       <nav className="w-full hidden mx-auto lg:flex justify-center items-center p-4 top-0 fixed z-50  text-nowrap">
+        <motion.div
+          style={{ opacity }}
+          ref={serviceMenuRef}
+          className="flex flex-wrap items-stretch p-4 max-w-[1040px] mx-auto justify-center gap-4 absolute inset-[auto_0_0_0] translate-y-full bg-[#FFFFFFCC] rounded-xl backdrop-blur-md"
+        >
+          <ServiceLink to="Hull Cleaning" />
+          <ServiceLink to="Remotely Operated Vehicle (ROV)" />
+          <ServiceLink to="Procurement and equipment rental" />
+          <ServiceLink to="Offshore support" />
+          <ServiceLink to="Offshore support" />
+        </motion.div>
         <div
           className="w-full max-w-[1040px] [backdrop-filter:blur(320px)] rounded-full shadow border border-white 
        bg-white/50 overflow-hidden flex flex-row items-center justify-between py-3 px-4 pl-6  box-border text-center text-xl text-foundation-grey-grey-500 h-max"
@@ -44,17 +82,17 @@ export default function Navbar() {
             >
               About us
             </Link>
-            <Link
-              href="/Services"
+            <div
+              onClick={() => setAreServicesOpen((prev) => !prev)}
               className={twMerge(
-                'hover:text-foundation-rust-accent-rust-accent-500 no-underline',
-                pathname === '/Services'
+                'hover:text-foundation-rust-accent-rust-accent-500 relative no-underline cursor-pointer',
+                areServicesOpen
                   ? 'font-semibold text-foundation-rust-accent-rust-accent-500'
                   : ''
               )}
             >
               Our services
-            </Link>
+            </div>
             <Link
               href="/stories"
               className={twMerge(
@@ -78,7 +116,6 @@ export default function Navbar() {
               Gallery
             </Link>
           </div>
-
           <motion.div
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
@@ -91,7 +128,6 @@ export default function Navbar() {
               Let&apos;s talk
             </Link>
           </motion.div>
-
           <button onClick={toggleMenu} className="md:hidden">
             {!isMenuOpen ? (
               <FiMenu className="w-6 h-6" />
@@ -104,3 +140,16 @@ export default function Navbar() {
     </>
   );
 }
+
+const ServiceLink = function ({ to }: { to: string }) {
+  return (
+    <div className="grid items-center grid-cols-[1fr_auto] grid-rows-[1fr] bg-white gap-2 p-6 rounded-2xl text-[#1E1E1E] font-clash font-medium w-[313px] justify-between">
+      <p className="row-start-1 col-start-1 col-end-2 break-words whitespace-normal text-lg">
+        {to}
+      </p>
+      <div className="w-[35px] h-[35px] row-start-1 col-start-2 col-end-3 rounded-full border border-[#1E1E1E] grid place-content-center">
+        <HiMiniArrowUpRight className="text-[#1E1E1E] text-2xl" />
+      </div>
+    </div>
+  );
+};
