ddcb39692e29398c2526483f0635ef6a2da8750f
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-10-01 15:40:25 +0100

    chore: add optional classname prop to smallie

--- app/components/Smallie.tsx
+++ app/components/Smallie.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface SmallieProps {
-  text: string
+  text: string;
+  className?: string;
 }
 
-function Smallie({ text }: SmallieProps) {
+function Smallie({ text, className = '' }: SmallieProps) {
   return (
-    <div className="w-[362px] ">
+    <div className={twMerge('w-[362px]', className)}>
       <p className=" text-[0.875rem] sm:text-[1rem] sm:tracking-[0.03em] uppercase font-medium font-clash text-foundation-grey-grey-100 text-left ">
         {text}
       </p>
@@ -14,4 +16,4 @@ function Smallie({ text }: SmallieProps) {
   );
 }
 
-export default Smallie
+export default Smallie;
