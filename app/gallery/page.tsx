2ef169cc18c496e0706e0a53d93b2ef43c0b791b
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-10-08 15:33:51 +0100

    feat: added extra images

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -9,8 +9,7 @@ function page() {
       className="pt-[20rem] px-[1.5rem] md:px-[2.5rem] lg:px-[5rem] pb-[2rem] sm:pb-[2.5rem] md:pb-[4rem]
        max-w-[1440px] w-full mx-auto gap-[2rem] sm:gap-[2.55rem] lg:gap-[7.5rem] text-left"
     >
-      <div className="sm:grid grid-rows-[repeat(6,_auto)] grid-cols-[repeat(3,_auto)]">
-
+      <div className="sm:grid grid-rows-[repeat(9,_auto)] grid-cols-[repeat(3,_auto)]">
         <Smallie
           text="pictures would convince you"
           className="col-start-1 col-end-2 row-start-1 row-end-2 "
@@ -78,6 +77,39 @@ function page() {
               alt="factory"
               width={427}
               height={600}
+            />
+          </ScrollFade>
+        </ImageReveal>
+        <ImageReveal className="col-start-2 col-end-3 row-start-7 row-end-8">
+          <ScrollFade>
+            <Image
+              className="w-full mt-6"
+              src="/gallery/img4.png"
+              alt="factory"
+              width={428}
+              height={600}
+            />
+          </ScrollFade>
+        </ImageReveal>
+        <ImageReveal className="col-start-1 col-end-2 row-start-8 row-end-9">
+          <ScrollFade>
+            <Image
+              className="w-full mt-6"
+              src="/gallery/img2.png"
+              alt="factory"
+              width={427}
+              height={300}
+            />
+          </ScrollFade>
+        </ImageReveal>
+        <ImageReveal className="col-start-3 col-end-4 row-start-9 row-end-10">
+          <ScrollFade>
+            <Image
+              className="w-full mt-6"
+              src="/gallery/img3.png"
+              alt="factory"
+              width={428}
+              height={300}
             />
           </ScrollFade>
         </ImageReveal>
