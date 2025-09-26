b04a8668b1be610e1a19837fc245b3bb15092e7b
Bitsaac <120939905+AviOfLagos@users.noreply.github.com>
2025-09-26 21:07:04 +0100

    Update: Awards

--- app/components/about/Awards.tsx
+++ app/components/about/Awards.tsx
@@ -8,9 +8,9 @@ interface BrandProps {
 const Brand: React.FC<BrandProps> = ({ text }) => {
   return (
     <div
-      className="w-[400px] sm:w-[400px] h-[9.25rem] flex relative leading-[140%] uppercase font-medium  p-[1.5rem] justify-center items-center
+      className="w-full h-[9.25rem] flex relative leading-[140%] uppercase font-medium  p-[1.5rem] justify-center items-center
       rounded-lg bg-foundation-white-white-500 hover:shadow hover:scale-110 transition-transform  border-foundation-grey-grey-50 border-[0.5px] border-solid box-border overflow-hidden
-       text-center text-[0.875rem] text-foundation-grey-grey-500 font-caption-1"
+       text-center text-xl text-foundation-grey-grey-500 font-caption-1"
     >
       {text}
     </div>
@@ -19,15 +19,13 @@ const Brand: React.FC<BrandProps> = ({ text }) => {
 
 const Brands: React.FC = () => {
   const items = [
-    'IECEx (International Electrotechnical Commission)',
-    'SOLAS (Safety of Life at Sea)',
-    'ATEX (Atmospheres Explosible)',
-    'IMO (International Maritime Organization)',
+    'ISO 45001:2018 Cerified',
+    'ISO 9001:2015 Certified',
   ];
 
   return (
     <div
-      className="w-full max-w-[1440px] items-center justify-center flex flex-col py-[7.5rem] 
+      className="w-full max-w-[1440px] items-start justify-center flex flex-col py-[7.5rem] 
        px-[1.5rem] md:px-[2.5rem] lg:px-[5rem] pb-[2.5rem] 
         gap-[2rem] md:gap-[2.5rem] overflow-hidden "
     >
@@ -35,11 +33,11 @@ const Brands: React.FC = () => {
         text="Awards & certifications"
         className="w-full text-center flex justify-center"
       />
-      <div className="  flex gap-4 w-full sm:gap-8 py-4   ">
-        {[...items, ...items].map((item, index) => (
+      <div className="  flex gap-2 w-full sm:gap-4 py-4   ">
+        {[...items].map((item, index) => (
           <div
             key={index}
-            className="flex w-full relative  "
+            className="flex w-full px-4 relative  "
             style={{
               animation: `scroll 50s linear infinite`,
               animationDelay: `${index * 100}ms`,
